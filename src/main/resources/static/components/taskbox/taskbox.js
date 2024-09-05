439c6cd42efbb993984d6bbaa5ac14385a773112
h598062 <111509904+h598062@users.noreply.github.com>
2024-09-05 17:55:56 +0200

    add taskbox template

---
+++ src/main/resources/static/components/taskbox/taskbox.js
@@ -0,0 +1,18 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <link rel="stylesheet" type="text/css"
+        href="${import.meta.url.match(/.*\//)[0]}/taskbox.css"/>
+    <dialog>
+        <!-- Modal content -->
+        <span>&times;</span>
+        <div>
+            <div>Title:</div>
+            <div>
+                <input type="text" size="25" maxlength="80"
+                    placeholder="Task title" autofocus/>
+            </div>
+            <div>Status:</div><div><select></select></div>
+        </div>
+        <p><button type="submit">Add task</button></p>
+    </dialog>
+`;
